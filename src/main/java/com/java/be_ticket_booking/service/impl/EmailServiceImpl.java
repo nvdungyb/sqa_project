57747579c63c4e37254a2784ae22fe138277c2fb
NvDzung <dung3032003md@gmail.com>
2025-04-10 22:35:01 +0700

    Add getter and setter for EmailServiceImpl

--- src/main/java/com/java/be_ticket_booking/service/impl/EmailServiceImpl.java
+++ src/main/java/com/java/be_ticket_booking/service/impl/EmailServiceImpl.java
@@ -1,31 +1,46 @@
 package com.java.be_ticket_booking.service.impl;
 
+import com.java.be_ticket_booking.service.EmailService;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
-import com.java.be_ticket_booking.service.EmailService;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
+@Setter
+@Getter
 public class EmailServiceImpl implements EmailService {
-	
-	@Value("${app.default_sender}")
-	private String default_sender;
-	
-	@Autowired
-	private JavaMailSender mailSender;
 
-	@Override
-	public void sendMail(String toMail, String subject, String body) {
-		SimpleMailMessage mail = new SimpleMailMessage();
-		mail.setFrom(default_sender);
-		mail.setTo(toMail);
-		mail.setSubject(subject);
-		mail.setText(body);
-		
-		mailSender.send(mail);
-	}
+    @Value("${app.default_sender}")
+    private String default_sender;
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Override
+    public void sendMail(String toMail, String subject, String body) {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setFrom(default_sender);
+        mail.setTo(toMail);
+        mail.setSubject(subject);
+        mail.setText(body);
+
+        mailSender.send(mail);
+    }
+
+    public void setDefault_sender(String default_sender) {
+        this.default_sender = default_sender;
+    }
+
+    public String getDefault_sender() {
+        return default_sender;
+    }
+
+    public void setMailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 }
