d94b6e7bf2841c32045e2929c2109ebf9da491f7
NvDzung <dung3032003md@gmail.com>
2025-04-24 09:10:18 +0700

    Enable scheduling support by adding @EnableScheduling annotation

--- src/main/java/com/java/be_ticket_booking/BeTicketBookingApplication.java
+++ src/main/java/com/java/be_ticket_booking/BeTicketBookingApplication.java
@@ -2,8 +2,10 @@ package com.java.be_ticket_booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BeTicketBookingApplication {
 
     public static void main(String[] args) {
